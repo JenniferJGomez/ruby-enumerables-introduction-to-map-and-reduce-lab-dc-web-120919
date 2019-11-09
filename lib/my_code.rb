9f031b4611932603a1f2f68eeeeb2da167a93dc3
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-09 09:31:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,11 +37,11 @@ end
 
 def reduce_to_total(source_array, starting_point)
 
-  new = (starting_point = 0) 
-  i = 0 
+  new = starting_point
+  i = 0
   while i < source_array.length do
     new += source_array[i]
-    i += 1 
+    i += 1
   end
-new 
+  return new
 end
